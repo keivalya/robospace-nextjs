fcaf37a3427feed8f9e0ab617ee365d7988e0edc
keivalya <keivalyapandya2001@gmail.com>
2025-08-07 12:27:10 -0400

    added clarity

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,24 @@ import Pricing from '@/components/Pricing';
 import Footer from '@/components/Footer';
 import LoginModal from '@/components/LoginModal';
 import SignupModal from '@/components/SignupModal';
+import Script from 'next/script';
 
 export default function Home() {
   return (
     <>
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sh6qul0g41");
+          `
+        }}
+      />
       <Navigation />
       <main>
         <Hero />
