3fd7a060176be998dbc7e50d25f6efa4e7243f53
keivalya <keivalyapandya2001@gmail.com>
2025-08-08 21:28:58 -0400

    debugged logged-out documentation on navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -73,6 +73,7 @@ export default function Navigation() {
           ) : (
             <>
               <Link href="/#features" onClick={closeMenu}>Features</Link>
+              <Link href="/documentation" onClick={closeMenu}>Documentation</Link>
               <Link href="/#about" onClick={closeMenu}>About</Link>
               <Link href="/#pricing" onClick={closeMenu}>Pricing</Link>
               <button 
